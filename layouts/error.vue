ad2169e153f21fe925c3fc85b86a007d0d8d9784
Ovi Savescu <ovi@ovswebsites.com>
2018-10-26 09:00:08 +0300

    temp padded error content for visibility

---
+++ layouts/error.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <p>111111111111111111111111111</p>
+    <p>111111111111111111111111111</p>
+    <p>111111111111111111111111111</p>
+    <p>111111111111111111111111111</p>
+    <p>111111111111111111111111111</p>
+    <p>111111111111111111111111111</p>
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <nuxt-link to="/">Home page</nuxt-link>
+    <p>111111111111111111111111111</p>
+    <p>111111111111111111111111111</p>
+    <p>111111111111111111111111111</p>
+    <p>111111111111111111111111111</p>
+    <p>111111111111111111111111111</p>
+    <p>111111111111111111111111111</p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: () => {
+        return { message: "Unknown error" }
+      }
+    }
+  },
+  layout: "blog" // you can set a custom layout for the error page
+}
+</script>
